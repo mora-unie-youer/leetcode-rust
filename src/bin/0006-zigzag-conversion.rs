a5bb453f3fbbd95cc01b979a805f808d684e9084
Mora Unie Youer <mora_unie_youer@riseup.net>
2023-07-30 18:47:50 +0000

    Solution for `0006`

---
+++ src/bin/0006-zigzag-conversion.rs
@@ -0,0 +1,74 @@
+/**
+ * 6. Zigzag Conversion
+ *
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display
+ * this pattern in a fixed font for better legibility)
+ *   P   A   H   N
+ *   A P L S I I G
+ *   Y   I   R
+ * And then read line by line: "PAHNAPLSIIGYIR"
+ *
+ * Write the code that will take a string and make this conversion given a number of rows:
+ * string convert(string s, int numRows);
+ *
+ *
+ * Example 1:
+ *   Input: s = "PAYPALISHIRING", numRows = 3
+ *   Output: "PAHNAPLSIIGYIR"
+ *
+ * Example 2:
+ *   Input: s = "PAYPALISHIRING", numRows = 4
+ *   Output: "PINALSIGYAHRPI"
+ *   Explanation:
+ *   P     I    N
+ *   A   L S  I G
+ *   Y A   H R
+ *   P     I
+ *
+ * Example 3:
+ *   Input: s = "A", numRows = 1
+ *   Output: "A"
+ *
+ * Constraints:
+ *   1 <= s.length <= 1000
+ *   s consists of English letters (lower-case and upper-case), ',' and '.'.
+ *   1 <= numRows <= 1000
+ */
+pub struct Solution;
+fn main() {}
+
+// SUBMISSION CODE START
+
+impl Solution {
+    pub fn convert(s: String, num_rows: i32) -> String {
+        let mut chars: Vec<(i32, char)> = (0..num_rows)
+            .chain((1..num_rows - 1).rev())
+            .cycle()
+            .zip(s.chars())
+            .collect();
+        chars.sort_by_key(|&(row, _)| row);
+        chars.into_iter().map(|(_, c)| c).collect()
+    }
+}
+
+// SUBMISSION CODE END
+
+#[cfg(test)]
+mod tests {
+    use crate::Solution;
+
+    #[test]
+    fn test_examples() {
+        assert_eq!(
+            Solution::convert("PAYPALISHIRING".into(), 3),
+            "PAHNAPLSIIGYIR"
+        );
+
+        assert_eq!(
+            Solution::convert("PAYPALISHIRING".into(), 4),
+            "PINALSIGYAHRPI"
+        );
+
+        assert_eq!(Solution::convert("A".into(), 1), "A");
+    }
+}
