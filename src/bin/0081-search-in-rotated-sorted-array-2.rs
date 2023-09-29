3a6e09a3805696d9c5729d5cecb4624dacca7b73
Mora Unie Youer <mora_unie_youer@riseup.net>
2023-09-29 10:21:25 +0000

    Solution for `0081`

---
+++ src/bin/0081-search-in-rotated-sorted-array-2.rs
@@ -0,0 +1,63 @@
+/**
+ * 81. Search in Rotated Sorted Array II
+ *
+ * There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+ *
+ * Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the
+ * resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example,
+ * [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
+ *
+ * Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in
+ * nums.
+ *
+ * You must decrease the overall operation steps as much as possible.
+ *
+ *
+ * Example 1:
+ *   Input: nums = [2,5,6,0,0,1,2], target = 0
+ *   Output: true
+ *
+ * Example 2:
+ *   Input: nums = [2,5,6,0,0,1,2], target = 3
+ *   Output: false
+ *
+ *  
+ *
+ * Constraints:
+ *   1 <= nums.length <= 5000
+ *   -104 <= nums[i] <= 104
+ *   nums is guaranteed to be rotated at some pivot.
+ *   -104 <= target <= 104
+ *
+ *
+ * Follow up: This problem is similar to Search in Rotated Sorted Array, but nums may contain duplicates. Would this affect
+ * the runtime complexity? How and why?
+ */
+pub struct Solution;
+fn main() {}
+
+// SUBMISSION CODE START
+
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> bool {
+        let nums = &nums[nums
+            .iter()
+            .position(|n| n != nums.last().unwrap())
+            .unwrap_or(nums.len() - 1)..];
+        let pivot = nums.partition_point(|n| n > nums.last().unwrap());
+        nums[..pivot].binary_search(&target).is_ok() || nums[pivot..].binary_search(&target).is_ok()
+    }
+}
+
+// SUBMISSION CODE END
+
+#[cfg(test)]
+mod tests {
+    use crate::Solution;
+
+    #[test]
+    fn test_examples() {
+        assert!(Solution::search(vec![2, 5, 6, 0, 0, 1, 2], 0));
+        assert!(!Solution::search(vec![2, 5, 6, 0, 0, 1, 2], 3));
+    }
+}
