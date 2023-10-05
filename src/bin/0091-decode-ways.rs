ff0b096427a48706ef25463121adbefce7a09315
Mora Unie Youer <mora_unie_youer@riseup.net>
2023-10-05 17:12:55 +0000

    Solution for `0091`

---
+++ src/bin/0091-decode-ways.rs
@@ -0,0 +1,85 @@
+/**
+ * 91. Decode Ways
+ *
+ * A message containing letters from A-Z can be encoded into numbers using the following mapping:
+ *
+ * 'A' -> "1"
+ * 'B' -> "2"
+ * ...
+ * 'Z' -> "26"
+ *
+ * To decode an encoded message, all the digits must be grouped then mapped back into letters using the reverse of the
+ * mapping above (there may be multiple ways). For example, "11106" can be mapped into:
+ *   "AAJF" with the grouping (1 1 10 6)
+ *   "KJF" with the grouping (11 10 6)
+ *
+ * Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into 'F' since "6" is different from "06".
+ *
+ * Given a string s containing only digits, return the number of ways to decode it.
+ *
+ * The test cases are generated so that the answer fits in a 32-bit integer.
+ *
+ *
+ * Example 1:
+ *   Input: s = "12"
+ *   Output: 2
+ *   Explanation: "12" could be decoded as "AB" (1 2) or "L" (12).
+ *
+ * Example 2:
+ *   Input: s = "226"
+ *   Output: 3
+ *   Explanation: "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+ *
+ * Example 3:
+ *   Input: s = "06"
+ *   Output: 0
+ *   Explanation: "06" cannot be mapped to "F" because of the leading zero ("6" is different from "06").
+ *
+ *
+ * Constraints:
+ *   1 <= s.length <= 100
+ *   s contains only digits and may contain leading zero(s).
+ */
+pub struct Solution;
+fn main() {}
+
+// SUBMISSION CODE START
+
+impl Solution {
+    pub fn num_decodings(s: String) -> i32 {
+        let s = s.as_bytes();
+        let n = s.len();
+        let (mut prev, mut curr) = (1, (s[0] != b'0') as i32);
+
+        for i in 2..=n {
+            let prev_ch = s[i - 2];
+            let curr_ch = s[i - 1];
+
+            let one = if curr_ch != b'0' { curr } else { 0 };
+            let two = if prev_ch == b'1' || prev_ch == b'2' && curr_ch <= b'6' {
+                prev
+            } else {
+                0
+            };
+
+            prev = curr;
+            curr = one + two;
+        }
+
+        curr
+    }
+}
+
+// SUBMISSION CODE END
+
+#[cfg(test)]
+mod tests {
+    use crate::Solution;
+
+    #[test]
+    fn test_examples() {
+        assert_eq!(Solution::num_decodings("12".into()), 2);
+        assert_eq!(Solution::num_decodings("226".into()), 3);
+        assert_eq!(Solution::num_decodings("06".into()), 0);
+    }
+}
